d9f24c5776a2beb6d3d4d4a98afca1ed5f9024c3
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-28 16:45:52 +0200

    Ciclo while userChoice

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,7 +1,14 @@
 "use strict";
 
 const userChoice = prompt("Scegli Pari o Dispari? Digita 'pari' o 'dispari'");
+
+while(userChoice != "pari" || userChoice != "dispari"){
+    const userChoice = prompt("Scegli Pari o Dispari? Digita 'pari' o 'dispari'");
+}
+
 let userNumber = prompt("Inserisci un numero da 1 a 5");
+
+let num = 0;
 
 function rollDice(num){
     const a = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
