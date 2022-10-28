27eb97338c63cdf1d40a5c5f2c24fd6e4dba924c
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-28 16:39:48 +0200

    Fix

---
+++ Pari e Dispari/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const userChoice = prompt("Scegli Pari o Dispari? Digita 'pari' o 'dispari'");
+let userNumber = promp("Inserisci un numero da 1 a 5");
+
+function rollDice(num){
+    const a = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+
+    return num;
+}
+
+console.log(num);
